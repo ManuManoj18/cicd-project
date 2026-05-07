1a7322863cbef139cab01a883313b84b978b3ed6
ManuManoj18 <manumanojb18@gmail.com>
2026-05-07 18:52:40 +0530

    Testing automatic CI/CD update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("🚀 CI/CD Pipeline is Working!");
+    res.send("✅ CI/CD Automatic Update Successful!");
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
